13780926b0485660277a9c0276e23b0e94556656
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-05-29 16:34:46 +0200

    Prepare for 0.13 version

--- HTML.pm
+++ HTML.pm
@@ -9,7 +9,7 @@ use Error::Pure qw(err);
 use Plack::Util::Accessor qw(component constructor_args data data_css data_init);
 use Symbol::Get;
 
-our $VERSION = 0.12;
+our $VERSION = 0.13;
 
 sub _css {
 	my $self = shift;
@@ -309,6 +309,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.12
+0.13
 
 =cut
