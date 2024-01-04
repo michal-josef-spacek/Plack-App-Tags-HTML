d170db908b32cdd841cf66d99b8294b0d0b286ec
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-01-04 01:24:10 +0100

    Prepare for 0.16 version

--- HTML.pm
+++ HTML.pm
@@ -9,7 +9,7 @@ use Error::Pure qw(err);
 use Plack::Util::Accessor qw(component constructor_args data data_css data_init data_prepare);
 use Symbol::Get;
 
-our $VERSION = 0.15;
+our $VERSION = 0.16;
 
 sub _css {
 	my ($self, $env) = @_;
@@ -328,6 +328,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.15
+0.16
 
 =cut
